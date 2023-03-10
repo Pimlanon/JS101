239b378f5ba65347fad033ae8cba750990912ae8
Pimlanon <nonlaphan.pim@gmail.com>
2023-03-10 13:40:51 +0700

    add number

--- task2/task2.js
+++ task2/task2.js
@@ -1,12 +1,27 @@
+// string
 let firstName = 'Lak';
 let lastName = 'Donovan';
 
 let fulName = firstName + '' + lastName;
 console.log(fulName);
 
+// Boolean
 let info = `My name is ${fulName} and I am ${firstName}`; 
 console.log(info);
 
 let email = 'donovanlak@gmail.com';
 let findLetter = email.includes('@');
 console.log(findLetter);
+
+// number
+let radius = 10;
+const pi = 3.14;
+
+console.log(radius,pi);
+console.log(10/2);
+
+let result = radius % 3; //this will get remain
+console.log(result);
+
+let bidMas = 5 * (10-3)**2; //ไม่มี () จะเรียงจากซ้ายไปขวา | 5 * (10-3)**2 อันนี้ () ก่อน
+console.log(bidMas);
