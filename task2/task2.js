15103c72c1e853611c46426a4f4b5afebc298d6f
Pimlanon <nonlaphan.pim@gmail.com>
2023-03-10 13:33:14 +0700

    learn string

---
+++ task2/task2.js
@@ -0,0 +1,12 @@
+let firstName = 'Lak';
+let lastName = 'Donovan';
+
+let fulName = firstName + '' + lastName;
+console.log(fulName);
+
+let info = `My name is ${fulName} and I am ${firstName}`; 
+console.log(info);
+
+let email = 'donovanlak@gmail.com';
+let findLetter = email.includes('@');
+console.log(findLetter);
