98192a84685d259cbb43a9a6fd6edd410a12e5bb
Pimlanon <nonlaphan.pim@gmail.com>
2023-03-13 15:42:47 +0700

    compare declaration and expression

--- task3/task3.js
+++ task3/task3.js
@@ -6,7 +6,20 @@ if (buyerAge < 18) {
 }
 
 
-info ('Jack');
-function info(name){
-    console.log('Hello' + name);
+// --------------------------------------
+// declarations
+
+function info(name) {
+    console.log('Hello ' + name);
 }
+info ('Jack');
+
+// expression can prevent from a 'hoisting'
+const myInfo = function(name) {
+    console.log('Hello ' + name);
+};
+myInfo('Jill'); //<<<expression style need to declare value under function
+
+
+//---------------------------
+//arrow function use => to less coding
