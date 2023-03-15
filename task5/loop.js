f12ce5dad7d676030ea2c2aa071ad5ac78f5294e
Pimlanon <nonlaphan.pim@gmail.com>
2023-03-16 01:35:12 +0700

    completed

--- task5/loop.js
+++ task5/loop.js
@@ -1,17 +1,19 @@
-// for (let i = 0; i < 5 ; i++){
-//     console.log('Hello World' + i)
-// }
-
+for (let i = 0; i < 5 ; i++){
+    console.log('Hello World' + i)
+}
+console.log('-------------');
 
 //ลูปเลขคี่ ให้น้อยกว่า 5
 for (let i = 0; i < 5 ; i++){
     if (i % 2 !== 0) console.log(`I am odd ` + i);
 }
+console.log('-------------');
 
 let arr = ["a", "b", "c"];
 for (let i = 0; i < arr.length; i++ ) {
     console.log(arr[i]);
 }
+console.log('----------------');
 
 for (let i = 0; i < 5; i++) {
     console.log(i);
@@ -22,5 +24,33 @@ for (let i = 0; i < 5; i++) {
 
 //for loop รู้ค่าตัวสุดท้าย
 //while loop ไม่รู้ค่าสุดท้าย เช่น ต้องรอลูกค้ามาบอกรหัสเกม 
-i = 0;
-while 
+
+for (let i = 0; i <= 4; i++) {
+    let temp = ''
+    for (let j = 0; j <= i; j++) {
+        temp += '*'
+    }
+    console.log(temp);
+    }
+    
+    console.log('-------------');
+
+    for (let i = 1; i <= 5; i++) {
+    let temp = ''
+    for (let j = 1; j <= i; j++) {
+        temp += '*'
+    }
+    console.log(temp);
+    }
+
+
+    function oddNumber(number) {
+        let i = 0
+        while (i <= number) {
+            if (i % 2 !== 0) {
+                console.log(i)
+            }
+            i++
+        }
+    }
+    oddNumber(50);
