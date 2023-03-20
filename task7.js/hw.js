5a375b4ac237bfc3ce20edf8bdcfba3324b7424a
Pimlanon <nonlaphan.pim@gmail.com>
2023-03-20 13:12:19 +0700

    completed

---
+++ task7.js/hw.js
@@ -0,0 +1,17 @@
+let chocMintLover = {
+    luck : 'S',
+    appearance : 'gorgeous',
+    charm: '100%',
+    quote() {
+        return ('ชาวช็อกมิ้นเลิฟเวอร์รสนิยมดีมาก!')
+    }
+};
+
+//console.log(chocMintLover); 
+//console.log(chocMintLover.quote());
+
+console.log(`Hello, Choc-mint lovers. Your luck is ${chocMintLover.luck}. Your appearance is ${chocMintLover.appearance}. 
+Your charm is ${chocMintLover.charm}, and I want to tell you that '${chocMintLover.quote()}'`)
+
+console.log('----------------------------------------------------------------');
+
