f252a83ae200254c15a1cb6191da8109d2ff5d58
Pimlanon <nonlaphan.pim@gmail.com>
2023-03-21 13:53:05 +0700

    add two example

---
+++ task 8/script.js
@@ -0,0 +1,14 @@
+const gJavascript = document.querySelector('#reading-list li:nth-child(2) .name');
+console.log(gJavascript);
+gJavascript.innerHTML = 'Advanced JAVASCRIPT!!';
+
+
+const tryHere = document.querySelector('#reading-list li:nth-child(1) .delete');
+console.log(tryHere);
+tryHere.innerHTML = 'Advanced ';
+
+//.textContent ใช้ได้เหมือน .innerHTML
+const readingList = document.querySelectorAll('#reading-list li .name');
+readingList.forEach(function(list){
+    list.textContent += 'book title: ';
+})
