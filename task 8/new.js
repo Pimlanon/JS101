c1ff8dd9634229092fd06917e821d9a843670917
Pimlanon <nonlaphan.pim@gmail.com>
2023-03-22 16:04:55 +0700

    first commit

---
+++ task 8/new.js
@@ -0,0 +1,19 @@
+function test() {
+    var a = "3";
+    var b = "8";
+    
+/***********Do not change the code above 👆*******/
+//Write your code on lines 7 - 9:
+
+a = 8-5;
+b = 3+5;
+
+console.log(a)
+/***********Do not change the code below 👇*******/
+
+console.log("a is " + a);
+console.log("b is " + b);
+}
+
+let tryMore = 243;
+console.log(tryMore)
