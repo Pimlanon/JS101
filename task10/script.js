01c181808ec37865c5e325a69b7c15c4bf7b9c49
Pimlanon <nonlaphan.pim@gmail.com>
2023-03-23 13:45:00 +0700

    add second example

--- task10/script.js
+++ task10/script.js
@@ -1,15 +1,36 @@
-const promise = new Promise ((resolve, reject) => {
-    let sum = 1+2;
+// const promise = new Promise ((resolve, reject) => {
+//     let sum = 1+2;
 
-    if (sum == 3){
-        resolve ('Success');
-    }else {
-        reject ('Failed');
-    }
-})
+//     if (sum == 3){
+//         resolve ('Success');
+//     }else {
+//         reject ('Failed');
+//     }
+// })
 
-promise.then((message) => {
-    console.log('This is success resolve ' + message)
-}).catch((message) => {
-    console.log('This is error reject ' + message)
-})
+// promise.then((message) => {
+//     console.log('This is success resolve ' + message)
+// }).catch((message) => {
+//     console.log('This is error reject ' + message)
+// })
+
+//////////////////////////////////////////////////////////////////////
+const failed = true;
+
+const myPromise = new Promise((resolve, reject) => { 
+    if(failed)
+        reject('fail promise');
+    else
+        resolve('resolved!!')
+});
+
+function success(result){
+  console.log(result);
+}
+
+function onFailed(error){
+  console.log(error);
+}
+
+myPromise.then((result) => success(result))
+.catch((error) => onFailed(error));
