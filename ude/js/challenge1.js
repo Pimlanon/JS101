a6c600db19c558a952edb762163d78f515dc4d49
Pimlanon <nonlaphan.pim@gmail.com>
2023-03-25 22:36:51 +0700

    completed challenge2

--- ude/js/challenge1.js
+++ ude/js/challenge1.js
@@ -5,10 +5,26 @@ let johnWeight = 92;
 let johnHeight = 1.95;
 
 let markBMI = markWeight / (markHeight ** 2);
-console.log(markBMI)
+console.log(markBMI) //27.309968138370508
 
 let johnBMI = johnWeight / (johnHeight ** 2);
-console.log(johnBMI)
+console.log(johnBMI) //24.194608809993426
 
 let markHigherBMI = markBMI > johnBMI
-console.log(markHigherBMI)
+console.log(markHigherBMI) //true
+
+//chalenge #2
+markWeight = 95;
+markHeight = 1.88;
+
+johnWeight = 85;
+johnHeight = 1.76;
+
+markBMI = markWeight / (markHeight ** 2);
+console.log(markBMI); //26.87867813490267
+
+johnBMI = johnWeight / (johnHeight ** 2);
+console.log(johnBMI) //27.44059917355372
+
+markHigherBMI = markBMI > johnBMI
+console.log(markHigherBMI) //false
