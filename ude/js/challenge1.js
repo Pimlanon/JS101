a3f81f1ad791ae2fb89b444bc0ab87d6bf3a6483
Pimlanon <nonlaphan.pim@gmail.com>
2023-03-26 22:57:26 +0700

    add more example

--- ude/js/challenge1.js
+++ ude/js/challenge1.js
@@ -1,33 +1,33 @@
-let markWeight = 78;
-let markHeight = 1.69;
+// let markWeight = 78;
+// let markHeight = 1.69;
 
-let johnWeight = 92;
-let johnHeight = 1.95;
+// let johnWeight = 92;
+// let johnHeight = 1.95;
 
-let markBMI = markWeight / (markHeight ** 2);
-console.log(markBMI) //27.309968138370508
+// let markBMI = markWeight / (markHeight ** 2);
+// console.log(markBMI) //27.309968138370508
 
-let johnBMI = johnWeight / (johnHeight * johnHeight);
-console.log(johnBMI) //24.194608809993426
+// let johnBMI = johnWeight / (johnHeight * johnHeight);
+// console.log(johnBMI) //24.194608809993426
 
-let markHigherBMI = markBMI > johnBMI
-console.log(markHigherBMI) //true
+// let markHigherBMI = markBMI > johnBMI
+// console.log(markHigherBMI) //true
 
-//chalenge1 exercise2
-markWeight = 95;
-markHeight = 1.88;
+// //chalenge1 exercise2
+// markWeight = 95;
+// markHeight = 1.88;
 
-johnWeight = 85;
-johnHeight = 1.76;
+// johnWeight = 85;
+// johnHeight = 1.76;
 
-markBMI = markWeight / (markHeight ** 2);
-console.log(markBMI); //26.87867813490267
+// markBMI = markWeight / (markHeight ** 2);
+// console.log(markBMI); //26.87867813490267
 
-johnBMI = johnWeight / (johnHeight * johnHeight);
-console.log(johnBMI) //27.44059917355372
+// johnBMI = johnWeight / (johnHeight * johnHeight);
+// console.log(johnBMI) //27.44059917355372
 
-markHigherBMI = markBMI > johnBMI
-console.log(markHigherBMI) //false
+// markHigherBMI = markBMI > johnBMI
+// console.log(markHigherBMI) //false
 
 ///////////////////////////////////////////////////
 //code challenge#3
@@ -71,7 +71,7 @@ if (scoreDolphins >= 100 && scoreDolphins === scoreKoalas) {
 ///////////////////////////////////////////////////
 
 //code challenge#4
-let billValue = 430;
+//let billValue = 430;
 /*
 if (billValue >= 50 && billValue <= 300) {
     console.log((billValue / 100) * 15);
@@ -80,10 +80,66 @@ if (billValue >= 50 && billValue <= 300) {
 }
 */
 // billValue / 100 * 15
-let tip = billValue >= 50 && billValue <= 300 ? (billValue / 100) * 15 : (billValue / 100) * 20
+/*let tip = billValue >= 50 && billValue <= 300 ? (billValue / 100) * 15 : (billValue / 100) * 20
 console.log(tip);
 let totalValue = billValue + tip;
 console.log('The bill was ' + billValue + ', the tip was ' + tip + ', and the total value ' + totalValue)
 // bill 275 : The bill was 275, the tip was 41.25, and the total value 316.25
 // bill 40: The bill was 40, the tip was 8, and the total value 48
 // bill 430: The bill was 430, the tip was 86, and the total value 516
+*/
+///////////
+// input [2, 4, 3]
+// output 29
+
+
+let input = [2, 3, 4]
+let initialValue = 0
+//let sqrtNum = arr => arr.reduce((acc,cur) => acc + cur ** 2, 0)
+let sqrtNum = arr => arr.reduce((acc,cur) => acc + Math.pow(cur,2), initialValue) //ได้เหมือนอันบน
+sqrtNum(input)
+
+console.log(sqrtNum(input))
+
+//-----------------------
+//แบบใช้ foreach
+const squareSum = (int) => {
+    let total = 0
+    int.forEach(e => total += e**2)
+    return total
+  }
+  squareSum(input)
+console.log(squareSum(input))
+
+//-----------------------
+const todos = [
+    { id: 1, title: 'reading' , completed: false},
+    { id: 2, title: 'workout' , completed: true},
+    { id: 3, title: 'cooking' , completed: true},
+    { id: 4, title: 'coding' , completed: false},
+  ]
+  
+  // output
+//   deleteTodo(2)
+//   [
+//     { id: 1, title: 'reading' , completed: false},
+//     { id: 3, title: 'cooking' , completed: true},
+//     { id: 4, title: 'coding' , completed: false},
+//   ]
+
+//ใช้ todo
+// function deleteTodo(n) {
+//     let temp = [...todos] //clonearray
+//     temp = temp.filter(todo => todo.id !== n);
+//     return temp;
+//   }
+//   deleteTodo(2)
+//   console.log(deleteTodo(2));
+
+  //ใช้ array
+  const deleteTodo = (arr,id) => {
+    let index = arr.findIndex(e => e.id === id)
+    arr.splice(index,1)
+    return arr;
+}
+console.log(deleteTodo(todos,2))
